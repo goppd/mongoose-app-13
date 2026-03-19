bc2c170536735f60ffc136587323e6453f30ba5e
goppd <goppdima@gmail.com>
2026-03-19 20:55:05 +0100

    Added models:Category-Product

---
+++ models/Product.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+  },
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+export default Product
